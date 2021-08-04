43b6352281a136e908b58c7bc003a9602048d86d
Jun Choi <cjh4549@gmail.com>
2021-08-03 21:25:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () { 
+    
+    // Array of greetings to randomize
+    const $greetings = {
+        time: ["Good Morning!", "Good Afternoon", "Good Night"]
+    }
+
+    // Time of the day
+    const $test = new Date();
+    const $hour = $test.getHours();
+
+    // Post submit animation
+    const $form = $('form').on('submit', function (e) {
+        e.preventDefault();
+
+        const $submitFill = $('.submit-fill');
+        $submitFill.css('z-index', '10');
+        $submitFill.toggleClass('changed')
+
+        const $username = $('input[type="text"]').val();
+
+        if ($hour < 12) {
+            $submitFill.html(`<h2 class="greeting">${$greetings.time[0]} ${$username}</h2>`)
+        } 
+        
+        else if ($hour >= 12 && $hour < 17) {
+            $submitFill.html(`<h2 class="greeting">${$greetings.time[1]} ${$username}</h2>`)
+        } 
+
+        else if ($hour >= 17 && $hour < 24) {
+            $submitFill.html(`<h2 class="greeting">${$greetings.time[2]} ${$username}</h2>`)
+        }
+    })
+})
+
+
